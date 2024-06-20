a4fc6b89d87c666681e19520c47694bce87c643f
LamND <lamnd@faboshop.vn>
2024-06-20 18:10:03 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/src/app",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/src/app/auth/login",
-        destination: "/auth/login",
-        permanent: true,
-      },
-      {
-        source: "/src/app/dashbroad",
-        destination: "/dashbroad",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
